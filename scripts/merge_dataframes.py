7c8a6951080330797ba3f714a55812dedc4d957a
clcarver1130 <carver.cameron@gmail.com>
2018-05-01 11:29:41 -0700

    merge_dataframes.py modified

---
+++ scripts/merge_dataframes.py
@@ -0,0 +1,20 @@
+# Load libraries
+import pandas as pd
+
+print('Loading data...')
+# Load in labeled data: df_labeled
+df_labeled = pd.read_csv('../raw_data/kiva_scraping.csv')
+
+# Load in unlabeled data: df_unlabel
+df_unlabel = pd.read_csv('..data/loans.csv')
+
+# Pull out just the 2010 data. Save it to a CSV: df_2010
+df_2010 = df_labeled[df_labeled['Funded Date.year'] == 2010]
+df_2010.to_csv('..data/df_2010.csv')
+
+print('Merging dataframes...')
+# Merge the datasets
+df_merged = df_2010.merge(df_unlabel, how='inner', left_on='id', right_on='LOAN_ID')
+df_merged.to_csv('..data/merged_df.csv')
+
+print("Done. 'df_2010' and 'df_merged' saved to data directory")

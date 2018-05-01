5bb791854cab5a7d570641149e1ad550c9b425fd
clcarver1130 <carver.cameron@gmail.com>
2018-05-01 13:17:48 -0700

    merge_dataframes.py saves to s3

--- scripts/merge_dataframes.py
+++ scripts/merge_dataframes.py
@@ -15,17 +15,17 @@ df_unlabel = pd.read_csv('../raw_data/loans.csv')
 
 # Pull out just the 2010 data. Save it to a CSV: df_2010
 df_2010 = df_labeled[df_labeled['Funded Date.year'] == 2010]
-file_2010 = df_2010.to_csv(None)
+string_2010 = df_2010.to_csv(None)
 file_2010 = bucket.new_key('df_2010.csv')
-file_2010.set_contents_from_string(file_2010)
+file_2010.set_contents_from_string(string_2010)
 print('df_2010 saved to s3')
 
 print('Merging dataframes...')
 # Merge the datasets
 df_merged = df_2010.merge(df_unlabel, how='inner', left_on='id', right_on='LOAN_ID')
-file_merge = df_merged.to_csv(None)
+string_merge = df_merged.to_csv(None)
 file_merge = bucket.new_key('df_merged.csv')
-file_merge.set_contents_from_string(file_merge)
+file_merge.set_contents_from_string(string_merge)
 print('df_merged saved to s3')
 
 print('Script Complete')

eb8703e7ade3f209fb6c510a8d5c20ea38167d7f
clcarver1130 <carver.cameron@gmail.com>
2018-05-01 11:45:01 -0700

    merge_dataframes.py modified

--- scripts/merge_dataframes.py
+++ scripts/merge_dataframes.py
@@ -10,11 +10,11 @@ df_unlabel = pd.read_csv('../raw_data/loans.csv')
 
 # Pull out just the 2010 data. Save it to a CSV: df_2010
 df_2010 = df_labeled[df_labeled['Funded Date.year'] == 2010]
-df_2010.to_csv('..data/df_2010.csv')
+df_2010.to_csv('../data/df_2010.csv')
 
 print('Merging dataframes...')
 # Merge the datasets
 df_merged = df_2010.merge(df_unlabel, how='inner', left_on='id', right_on='LOAN_ID')
-df_merged.to_csv('..data/merged_df.csv')
+df_merged.to_csv('../data/merged_df.csv')
 
 print("Done. 'df_2010' and 'df_merged' saved to data directory")

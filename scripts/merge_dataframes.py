0ae15fa9992862c4e974b9e8db3e79f587ee077c
clcarver1130 <carver.cameron@gmail.com>
2018-05-01 12:21:03 -0700

    modified to write files to s3

--- scripts/merge_dataframes.py
+++ scripts/merge_dataframes.py
@@ -1,5 +1,6 @@
 # Load libraries
 import pandas as pd
+import boto3
 
 print('Loading data...')
 # Load in labeled data: df_labeled
@@ -10,11 +11,16 @@ df_unlabel = pd.read_csv('../raw_data/loans.csv')
 
 # Pull out just the 2010 data. Save it to a CSV: df_2010
 df_2010 = df_labeled[df_labeled['Funded Date.year'] == 2010]
-df_2010.to_csv('../data/df_2010.csv')
+csv_buffer = StringIO()
+df_2010.to_csv(csv_buffer)
+s3_resource = boto3.resource('s3')
+s3_resource.Object(clcarver.kiva, 'df_2010.csv').put(Body=csv_buffer.getvalue())
 
 print('Merging dataframes...')
 # Merge the datasets
 df_merged = df_2010.merge(df_unlabel, how='inner', left_on='id', right_on='LOAN_ID')
-df_merged.to_csv('../data/merged_df.csv')
+df_merged.to_csv(csv_buffer)
+s3_resource = boto3.resource('s3')
+s3_resource.Object(clcarver.kiva, 'df_merged.csv').put(Body=csv_buffer.getvalue())
 
 print("Done. 'df_2010' and 'df_merged' saved to data directory")

0d26015cd6dc7f9a86f45d8df78b6612238261e3
Ubuntu <ubuntu@ip-172-31-42-63.us-west-2.compute.internal>
2018-05-04 23:16:57 +0000

    models tested on roc_curve

--- scripts/cluster_use.py
+++ scripts/cluster_use.py
@@ -6,15 +6,15 @@ from sklearn.cluster import KMeans
 
 
 # Connect to s3 bucket: clcarver.kiva
-conn = boto.connect_s3('SECRET KEYS')
+conn = boto.connect_s3('SECRET KEY')
 bucket = conn.get_bucket('clcarverloans')
 
 print('Loading data...')
 # Load in merged data: df_merged
-df_merged = pd.read_csv('https://s3.amazonaws.com/clcarverloans/data/df_merged.csv')
+df_cleaned = pd.read_csv('https://s3.amazonaws.com/clcarverloans/data/df_cleaned.csv')
 
 # Select loan 'Use' column: loan_use
-loan_use = df_merged['Use'].dropna() # Drop the one missing 'Use' datapoint
+loan_use = df_cleaned['Use'].dropna() # Drop the one missing 'Use' datapoint
 
 print('Vectorizing Description...')
 # Create vectorizor and fit_transfrom 'loan_use': vectorizor, x
@@ -28,13 +28,13 @@ km = KMeans(n_clusters=8)
 km.fit(x)
 
 # Merged Clusters into df_merged: df_cluster
-df_cluster = pd.concat([df_merged, pd.DataFrame({'Cluster': km.labels_})], axis=1)
+df_cluster = pd.concat([df_cleaned, pd.DataFrame({'Cluster': km.labels_})], axis=1)
 df_cluster.drop(columns='Unnamed: 0', inplace=True) # Drop extra index column
 print('New dataframe created')
 
 # Save to s3: df_cluster.csv
 string_cluster = df_cluster.to_csv(None)
-file_cluster = bucket.new_key('df_cluster.csv')
+file_cluster = bucket.new_key('data/df_cluster.csv')
 file_cluster.set_contents_from_string(string_cluster)
 print("File 'df_cluster' saved to s3")
 

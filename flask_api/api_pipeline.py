370267123a1e9ab61a41b7af619183df513fdd00
clcarver1130 <carver.cameron@gmail.com>
2018-05-15 15:14:05 -0700

    web form complete. still need to design

---
+++ flask_api/api_pipeline.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import pickle
+import numpy as np
+import requests
+import csv
+
+def predict_default(json):
+    # Data from web form is submitted: df
+    df = pd.DataFrame(json.to_dict(flat=False))
+    df.drop(columns='button', inplace=True)
+
+    # 'Loan Use' text is processed and its predicted cluster is added to df
+    with open('../scripts/cluster_model.pkl', 'rb') as input:
+        cluster_model = pickle.load(input)
+
+    with open('../scripts/vectorizer.pkl', 'rb') as input:
+        vectorizer = pickle.load(input)
+
+    use = vectorizer.transform(df['Use'])
+    cluster = cluster_model.predict(use)[0]
+    df['Cluster'] = cluster
+    df.drop(columns='Use', inplace=True)
+
+    # Dummify form submission: df_dummies
+    df_dummies = pd.get_dummies(df)
+
+    # Read in list of columns from trained model: full_dummies
+    with open('../data/full_col.csv', 'r') as f:
+        reader = csv.reader(f)
+        full_dummies = list(reader)
+
+    # Add in missing cols with a value of zero to df_dummies: final_df
+    missing_cols = set(full_dummies[0]) - set(df_dummies.columns.values)
+    for c in missing_cols:
+        df_dummies[c] = 0
+    final_df = df_dummies[full_dummies[0]]
+    final_df.drop(columns='Delinquent', inplace=True)
+
+    # Unpack our model
+    with open('../scripts/final_model.pkl', 'rb') as input:
+        model = pickle.load(input)
+        pred = model.predict_proba(final_df)[:, 1]
+        pred_str = str(pred[0] * 100) + '%'
+
+    return pred_str
--- flask_api/app.py
+++ flask_api/app.py
@@ -1,11 +1,21 @@
 
 from flask import Flask, render_template, request
+import pickle
+from api_pipeline import predict_default
+
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def form():
-    return render_template('index.html')
+    '''Render a page containing a form  where the user can input the borrower information'''
+    return render_template('form.html')
+
+@app.route('/predict', methods = ['POST', 'GET'])
+def predict():
+    data = request.form
+    pred = predict_default(data)
+    return render_template('predict.html', prediction=pred)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5008)
